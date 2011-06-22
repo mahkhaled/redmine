6c06dae5de876b9c9f00a47339112697156cceb7
Mostafa Ali <mostafa.ali@badrit.com>
2011-06-22 11:05:45 +0300

    inline edit issues

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -23,7 +23,7 @@ class IssuesController < ApplicationController
   before_filter :find_issues, :only => [:bulk_edit, :bulk_update, :move, :perform_move, :destroy]
   before_filter :check_project_uniqueness, :only => [:move, :perform_move]
   before_filter :find_project, :only => [:new, :create]
-  before_filter :authorize, :except => [:index]
+  before_filter :authorize, :except => [:index, :inline_issues]
   before_filter :find_optional_project, :only => [:index]
   before_filter :check_for_default_issue_status, :only => [:new, :create]
   before_filter :build_new_issue_from_params, :only => [:new, :create]
